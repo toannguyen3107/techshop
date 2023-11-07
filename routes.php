66d52be1c7557c245cbb696a5fef10cfa6e2862c
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-07 12:18:47 +0700

    fix rroutes

--- routes.php
+++ routes.php
@@ -11,9 +11,9 @@
     }
     $controllers = array('pages'=>['home', 'error']);
     if(array_key_exists($controller, $controllers)){
-        if(in_array($action, $controllers[$controller])){
+        if (in_array($action, $controllers[$controller])) {
             call($controller, $action);
-        }else{
+        } else {
             call('pages', 'error');
         }
     }else {
