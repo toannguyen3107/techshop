3991e7bca7cd3e1117736b7ba15037e89aec9d96
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-07 12:28:47 +0700

    fix - v3

--- app/view/templete.php
+++ app/view/templete.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME | TECHSHOP</title>
-    <?php require_once('../header.php'); ?>
+    <title>template</title>
+    <?php require('../header.php'); ?>
 </head>
 
 <body>
@@ -13,7 +13,8 @@
         <a href="#">HOME</a>
         <a href="#">ABOUT</a>
     </header>
-    <?php require_once('routes.php'); ?>
+
+    <?php require('routes.php'); ?>
     <!--  -->
     <footer>Copyright</footer>
 </body>
