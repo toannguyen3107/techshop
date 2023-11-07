1fd1c1814e3defe424a2d845508944b5bb389516
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-07 11:35:41 +0700

    first commit

---
+++ app/view/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME | TECHSHOP</title>
+    <?php require_once('../header.php'); ?>
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline text-clifford">
+        Hello world!
+    </h1>
+</body>
+
+</html>
