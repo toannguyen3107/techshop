8c31c90efa37cbe832aad461140c1ca9e53e789f
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-07 12:30:00 +0700

    fix - v3

--- app/view/templete.php
+++ app/view/templete.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>template</title>
-    <?php require('../header.php'); ?>
+    <?php require('app/header.php'); ?>
 </head>
 
 <body>
