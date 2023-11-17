0d1b177c52c86a7762afb1e23385a711eb4c3f8c
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-18 02:26:01 +0700

    update nav

--- app/component/nav.php
+++ app/component/nav.php
@@ -1,38 +1,59 @@
-<nav class="flex flex-row h-[100px] bg-[#49BF8C] gap-1">
+<nav class="flex flex-row h-[100px] bg-[#49BF8C] gap-3">
         <div class=" w-1/12 flex flex-row items-center justify-center">
-            <img src="/techshop/public/img/logo.png" alt="Logo techshop" class="h-[72px] w-[72px] rounded-full">
+            <img src="/techshop/public/img/large.png" alt="Logo techshop" class="h-[72px] w-[72px] rounded-full">
         </div>    
         <div class="w-1/12 my-auto">
-            <a href="#" class=" flex flex-row gap-1 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM144 448c0 8.8 7.2 16 16 16h64c8.8 0 16-7.2 16-16s-7.2-16-16-16H160c-8.8 0-16 7.2-16 16zM304 64H80V384H304V64z"/></svg>
-                <span class="text-lg">Điện thoại</span>
+            <a href="#" class=" flex flex-row gap-3 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center hover:border-2 hover:border-[#111]">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM144 448c0 8.8 7.2 16 16 16h64c8.8 0 16-7.2 16-16s-7.2-16-16-16H160c-8.8 0-16 7.2-16 16zM304 64H80V384H304V64z"/></svg>
+                <span class="text-base font-semibold">Điện thoại</span>
             </a>
         </div>
         <div class="w-1/12 my-auto">
-            <a href="#" class=" flex flex-row gap-1 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center">
+            <a href="#" class=" flex flex-row gap-3 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center hover:border-2 hover:border-[#111]">
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M128 32C92.7 32 64 60.7 64 96V352h64V96H512V352h64V96c0-35.3-28.7-64-64-64H128zM19.2 384C8.6 384 0 392.6 0 403.2C0 445.6 34.4 480 76.8 480H563.2c42.4 0 76.8-34.4 76.8-76.8c0-10.6-8.6-19.2-19.2-19.2H19.2z"/></svg>
-                <span class="text-lg">Laptop</span>
+                <span class="text-base font-semibold">Laptop</span>
             </a>
         </div>
         <div class="w-1/12 my-auto">
-            <a href="#" class="flex flex-row gap-1 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 64C0 28.7 28.7 0 64 0H384c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM256 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM384 64H64V384H384V64z"/></svg>
-                <span class="text-lg">Tablet</span>
+            <a href="#" class="flex flex-row gap-3 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center hover:border-2 hover:border-[#111]">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H384c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM256 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM384 64H64V384H384V64z"/></svg>
+                <span class="text-base font-semibold">Tablet</span>
             </a>
         </div>
         <div class="w-1/12 my-auto">
-            <a href="#" class="flex flex-row gap-1 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center">
-                <span class="text-lg">Khác</span>
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#162641}</style><path d="M384 480c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0zM224 352c-6.7 0-13-2.8-17.6-7.7l-104-112c-6.5-7-8.2-17.2-4.4-25.9s12.5-14.4 22-14.4l208 0c9.5 0 18.2 5.7 22 14.4s2.1 18.9-4.4 25.9l-104 112c-4.5 4.9-10.9 7.7-17.6 7.7z"/></svg>
+            <a href="#" class="flex flex-row gap-3 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center hover:border-2 hover:border-[#111]">
+                <span class="text-base font-semibold">Khác</span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><style>svg{fill:#162641}</style><path d="M384 480c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0zM224 352c-6.7 0-13-2.8-17.6-7.7l-104-112c-6.5-7-8.2-17.2-4.4-25.9s12.5-14.4 22-14.4l208 0c9.5 0 18.2 5.7 22 14.4s2.1 18.9-4.4 25.9l-104 112c-4.5 4.9-10.9 7.7-17.6 7.7z"/></svg>
             </a>
         </div>
+        <span class="my-auto text-4xl">|</span>
         <div class="w-1/12 my-auto text-center">
-            <a href="#" class="flex flex-col gap-1 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center">
-                <p>24h</p>
-                <p>Công nghệ</p>
+            <a href="#" class="flex flex-col gap-3 p-1 items-center hover:bg-[#fff0d4] hover:rounded-lg justify-center hover:border-2 hover:border-[#111]">
+                <p class="font-semibold">Tin tức</p>
             </a>
         </div>
-        <div class="w-1/12 my-auto">
-            <button type="button" class="bg-blue-500">Tài khoản<br>& Đơn hàng</button>
+        <span class="my-auto text-4xl">|</span>
+        <div class="w-2/12 flex justify-center my-auto">
+            <a href="#" class="w-4/5 flex flex-row justify-center items-center gap-[1rem] bg-[#DCCF55] hover:bg-[#dcad11] p-[1rem] rounded-full border-2 border-[#111] text-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+            <p>Đăng nhập</p>
+            </a>
+        </div>
+        <div class="w-2/12 flex justify-center my-auto">
+            <a href="#" class="w-4/5 flex flex-row justify-center items-center gap-[1rem] bg-[#DCCF55] hover:bg-[#dcad11] p-[1rem] rounded-full border-2 border-[#111] text-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+            <p>Đơn hàng</p> 
+            </a>
+        </div>
+        <span class="my-auto text-4xl">|</span>
+        <div class="w-3/12 flex flex-row justify-end items-center">
+            <div class="relative mr-5 w-[100%]">
+                <input type="text" placeholder="Bạn muốn mua gì?" class="w-[100%] p-[0.5rem] border-2 border-[#111] rounded-full">
+                <div  class="absolute right-[0.8rem] bottom-[0.8rem]">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                </div>
+                
+            </div>
+                
         </div>
 </nav>
--- app/view/home/index.php
+++ app/view/home/index.php
@@ -4,7 +4,10 @@
     <?php require_once '../app/component/head.php';?>
     <title>Home | TECHSHOP</title>
 </head>
-<body>
+<body class="overflow-x-hidden">
     <?php require_once '../app/component/nav.php'?>
+        <div class="mx-auto"><img src="/techshop/public/img/adv.png" alt="Adv main page" width="100%"></div>
+    <!-- footer -->
+    <?php require_once '../app/component/footer.php'?>
 </body>
 </html>
