83dc8bf467a8375ee518ca47af974764b17acee7
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-17 17:52:14 +0700

    toan

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/techshop/public/output.css">
     <title>Home | TECHSHOP</title>
 </head>
-<body>
-    <h1>hello <?=$data['name'];?></h1>
+<body >
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
