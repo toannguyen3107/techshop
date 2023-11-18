0e7257ecbbd89d664c9081ed5a444026f1f36164
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-18 11:28:41 +0700

    fix login style

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -6,12 +6,8 @@
 </head>
 <body class="overflow-x-hidden">
     <?php require_once '../app/component/nav.php'?>
-<<<<<<< HEAD
-    
-=======
         <div class="mx-auto"><img src="/techshop/public/img/adv.png" alt="Adv main page" width="100%"></div>
     <!-- footer -->
     <?php require_once '../app/component/footer.php'?>
->>>>>>> 0d1b177c52c86a7762afb1e23385a711eb4c3f8c
 </body>
 </html>
