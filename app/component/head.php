42cf357d116d1b7cab33442d07c9d1ffdc80a27d
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-28 12:42:52 +0700

    update local

--- app/component/head.php
+++ app/component/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://cdn.tailwindcss.com"></script>
+<script src="/techshop/public/tailwind.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.css"  rel="stylesheet" />
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
