bc3472bde40ce51da728ae6f7281e2327cb594e5
nguyenvanhau896 <nguyenvanhau896@gmail.com>
2023-12-02 11:24:19 +0700

    merge database into techshop.sql

--- techshop.sql
+++ techshop.sql
@@ -59,7 +59,7 @@ CREATE TABLE `product` (
 
 /* Phone Table */
 CREATE TABLE `phone` (
-  `id` INT PRIMARY KEY NOT NULL,
+  `id` INT,
   `kieumanhinh` VARCHAR(255),
   `dungluong` VARCHAR(255),
   FOREIGN KEY (`id`) REFERENCES `product`(`id`)
@@ -67,7 +67,7 @@ CREATE TABLE `phone` (
 
 /* Laptop Table */
 CREATE TABLE `laptop` (
-  `id` INT PRIMARY KEY NOT NULL,
+  `id` INT,
   `CPU` VARCHAR(255),
   `card` VARCHAR(255),
   `pin` VARCHAR(255),
@@ -77,13 +77,49 @@ CREATE TABLE `laptop` (
 
 /* Tablet Table */
 CREATE TABLE `tablet` (
-  `id` INT PRIMARY KEY NOT NULL,
+  `id` INT,
   `kieumanhinh` VARCHAR(255),
-  `phamtram_giam` INT,
   `dungluong` VARCHAR(255),
   `ketnoi` VARCHAR(255),
   FOREIGN KEY (`id`) REFERENCES `product`(`id`)
 );
+/* sample data */
+
+INSERT INTO `product` (`name`, `price`, `man_hinh`, `luotmua`, `created_at`, `updated_at`, `deleted`, `mo_ta`, `image`, `category`)
+VALUES
+('Iphone 14 Pro Max', 40080000, 'Super Retina XDR', 300, '2023-03-01 09:00:00', '2023-03-01 09:45:00', 0, 'The latest flagship iPhone with cutting-edge features', 'https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-max-tim-thumb-200x200.jpg', 'Phone'),
+('Dell XPS 13', 24990000, 'InfinityEdge', 90, '2023-03-02 12:30:00', '2023-03-02 13:15:00', 0, 'Premium ultrabook for productivity and entertainment', 'https://cdn.tgdd.vn/Products/Images/44/115447/dell-xps-13-9360-99h101-core-i7-7500u-8gb-256gb-2-450x300.jpg', 'Laptop'),
+('iPad Air', 15900000, 'Liquid Retina', 150, '2023-03-03 15:00:00', '2023-03-03 15:30:00', 0, 'Powerful and lightweight tablet for creative tasks', 'https://cdn.tgdd.vn/Products/Images/522/248096/ipad-air-5-wifi-grey-thumb-600x600.jpg', 'Tablet'),
+('Samsung Galaxy S21', 7600000, 'Dynamic AMOLED', 250, '2023-03-04 10:45:00', '2023-03-04 11:30:00', 0, 'High-performance Android smartphone with stunning display', 'https://cdn.tgdd.vn/Products/Images/42/267211/Samsung-Galaxy-S21-FE-vang-1-2-200x200.jpg', 'Phone'),
+('HP Spectre x360', 12800000, 'OLED', 80, '2023-03-05 14:00:00', '2023-03-05 14:45:00', 0, 'Convertible laptop with 4K display and powerful specs', 'https://cdn.tgdd.vn/Products/Images/44/202330/hp-spectre-x360-13-ap0087tu-i7-8565u-8gb-256gb-133-1-600x600.jpg', 'Laptop'),
+('Amazon Fire HD 10', 21600000, 'Full HD', 120, '2023-03-06 16:30:00', '2023-03-06 17:00:00', 0, 'Affordable tablet for entertainment and everyday use', 'https://www.techspot.com/images/products/2019/tablets/org/2019-11-15-product-3-j_1100.webp', 'Tablet'),
+('Google Pixel 6', 8900000, 'OLED', 180, '2023-03-07 11:15:00', '2023-03-07 11:45:00', 0, 'Flagship Google smartphone with impressive camera capabilities', 'https://m.media-amazon.com/images/I/61oQtjPpM-L.__AC_SX300_SY300_QL70_FMwebp_.jpg', 'Phone'),
+('Lenovo ThinkPad X1 Carbon', 11100000, 'IPS', 70, '2023-03-08 13:45:00', '2023-03-08 14:15:00', 0, 'Business ultrabook with a durable and lightweight design', 'https://cdn.tgdd.vn/Products/Images/44/299799/lenovo-thinkpad-e15-gen-4-i7-21e600cpvn-glr-a-1-180x125.jpg', 'Laptop'),
+('Microsoft Surface Pro 8', 25400000, 'PixelSense', 100, '2023-03-09 15:30:00', '2023-03-09 16:00:00', 0, 'Versatile 2-in-1 tablet/laptop with a high-resolution display', 'https://thegioigames.vn/wp-content/uploads/2022/10/microsoft-surface-pro-9-2-300x300.jpg', 'Tablet'),
+('OnePlus 9', 10500000, 'Fluid AMOLED', 200, '2023-03-10 09:30:00', '2023-03-10 10:00:00', 0, 'OnePlus flagship smartphone with fast performance', 'https://viostore.vn/wp-content/uploads/2023/08/oneplus-ace-2v-dimensity-9000-xanh.jpg.png', 'Phone'),
+('Acer Swift 5', 22890000, 'IPS', 60, '2023-03-11 12:00:00', '2023-03-11 12:30:00', 0, 'Thin and light laptop for on-the-go productivity', 'https://anphat.com.vn/media/product/42871_0357.jpg', 'Laptop'),
+('Huawei MatePad Pro', 3000000, 'OLED', 130, '2023-03-12 14:45:00', '2023-03-12 15:15:00', 0, 'Premium Android tablet with stylus support', 'https://dienthoaigiakho.vn/_next/image?url=https%3A%2F%2Fcdn.dienthoaigiakho.vn%2Fphotos%2F1666256303254-ippro-11-m2-3.jpg&w=1920&q=75', 'Tablet');
+
+INSERT INTO `phone` (`id`, `kieumanhinh`, `dungluong`)
+VALUES
+(1, 'Super AMOLED', '256GB'),
+(4, 'Dynamic AMOLED', '128GB'),
+(7, 'IPS LCD', '64GB'),
+(10, 'Super Retina XDR', '512GB');
+
+INSERT INTO `laptop` (`id`, `CPU`, `card`, `pin`, `khoiluong`)
+VALUES
+(2, 'AMD Ryzen 7', 'NVIDIA GTX 1660 Ti', '8 hours', '1.8 kg'),
+(5, 'Intel i5', 'Intel Iris Xe', '12 hours', '1.3 kg'),
+(8, 'AMD Ryzen 5', 'NVIDIA RTX 3050', '10 hours', '2.0 kg'),
+(11, 'Intel i9', 'NVIDIA RTX 3070', '6 hours', '2.5 kg');
+
+INSERT INTO `tablet` (`id`, `kieumanhinh`, `dungluong`, `ketnoi`)
+VALUES
+(3, 'AMOLED', '256GB', '4G LTE, Wi-Fi'),
+(6, 'IPS LCD', '128GB', 'Wi-Fi'),
+(9, 'Super Retina XDR', '512GB', '5G, Wi-Fi'),
+(12, 'TFT', '64GB', 'Wi-Fi, Bluetooth');
 
 /* Cart Table */
 CREATE TABLE `cart` (
